f6d6f45760f041a335816c7c4e39f8a5d925d524
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-05-25 15:19:03 +0200

    Add pep8 linter

--- server/app.py
+++ server/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from node_repository import Node, NodeRepository
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
 
 if __name__ == '__main__':
     node_repository = NodeRepository()
--- server/node_repository.py
+++ server/node_repository.py
@@ -1,12 +1,13 @@
 import sqlite3
 
+
 class Node:
     def __init__(self, id=None, ip=None):
         self.id = id
         self.ip = ip
 
     def __str__(self):
-        return str.format("Node(id={}, ip={})", self.id, self.ip)
+        return str.format('Node(id={}, ip={})', self.id, self.ip)
 
 
 class NodeRepository:
@@ -15,30 +16,39 @@ class NodeRepository:
 
     def create_database(self):
         with NodesConnection() as conn:
-            conn.execute('create table if not exists Nodes '\
-                        '(id INTEGER PRIMARY KEY AUTOINCREMENT, '\
-                        'ip TEXT NOT NULL)')
+            conn.execute(
+                'create table if not exists Nodes '
+                '(id INTEGER PRIMARY KEY AUTOINCREMENT, '
+                'ip TEXT NOT NULL)'
+            )
 
     def all(self):
         with NodesConnection() as conn:
-            return [ Node(id, ip) for (id, ip) in conn.execute('select id, ip from Nodes') ]
+            return [
+                Node(id, ip) for (id, ip)
+                in conn.execute('select id, ip from Nodes')
+            ]
 
     def find(self, id):
         with NodesConnection() as conn:
-            cmd = "select * from Nodes where id = ? limit 1"
+            cmd = 'select * from Nodes where id = ? limit 1'
             return conn.execute(cmd, id)[0]
 
     def create(self, node):
         with NodesConnection() as conn:
             cursor = conn.cursor()
-            cursor.execute("insert into Nodes(ip) values(?)", (node.ip,))
+            cursor.execute('insert into Nodes(ip) values(?)', (node.ip,))
             node.id = cursor.lastrowid
         return node
 
     def update(self, node):
         with NodesConnection() as conn:
-            conn.execute('update Nodes set ip = ? where id = ?', (node.ip, node.id))
+            conn.execute(
+                'update Nodes set ip = ? where id = ?',
+                (node.ip, node.id)
+                )
         return node
+
 
 class NodesConnection:
     def __init__(self):
