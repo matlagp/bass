c73fc377abca5a455048fad1bf24c159af1e9dc3
Piotr Matląg <piotr.matlag@protonmail.com>
2020-06-08 12:44:44 +0200

    PoC of streaming with RTOS tasks

--- sound-benchmarks/i2s-dac/src/main.cpp
+++ sound-benchmarks/i2s-dac/src/main.cpp
@@ -8,11 +8,31 @@
 
 #include "FreeRTOS.h"
 #include "WM8960.h"
+#include "driver/i2s.h"
 #include "freertos/ringbuf.h"
 #include "lwip/sockets.h"
 
 const char *ssid = "...";
 const char *pwd = "...";
+
+static const i2s_port_t i2s_num = I2S_NUM_0;  // i2s port number
+
+static const i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
+    .sample_rate = 44100,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, /* the DAC module will only
+                                                     take the 8bits from MSB */
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+    .intr_alloc_flags = 0,  // default interrupt priority
+    .dma_buf_count = 8,
+    .dma_buf_len = 64,
+    .use_apll = false};
+
+static const i2s_pin_config_t pin_config = {.bck_io_num = I2S_CLK,
+                                            .ws_io_num = I2S_WS,
+                                            .data_out_num = I2S_TXSDA,
+                                            .data_in_num = I2S_PIN_NO_CHANGE};
 
 bool connected = false;
 RingbufHandle_t buffer;
@@ -62,7 +82,7 @@ void tcpServerTask(void *pvParameters) {
     }
     Serial.println("Got connection");
 
-    xTaskCreate(dataIntakeTask, "DataIntake", 1 << 12, (void *)remoteSock, 1,
+    xTaskCreate(dataIntakeTask, "DataIntake", 1 << 15, (void *)remoteSock, 1,
                 NULL);
 
     // int len = recvfrom(sock, rxBuffer, sizeof(rxBuffer), 0,
@@ -87,7 +107,7 @@ void tcpServerTask(void *pvParameters) {
 
 void dataIntakeTask(void *pvParameters) {
   int *remoteSock = (int *)pvParameters;
-  byte rxData[512];
+  byte rxData[8192];
 
   while (true) {
     int len = recv(*remoteSock, rxData, sizeof(rxData), 0);
@@ -118,14 +138,26 @@ void i2sTask(void *pvParameters) {
   while (true) {
     size_t readSize;
     byte *data = (byte *)xRingbufferReceiveUpTo(buffer, &readSize,
-                                                pdMS_TO_TICKS(1000), 128);
+                                                pdMS_TO_TICKS(1000), 8192);
 
     if (data != NULL) {
       // Serial.printf("Read %d bytes:\n", readSize);
-      for (int i = 0; i < readSize; i++) {
-        printf("%c", data[i]);
-      }
+      // for (int i = 0; i < readSize; i++) {
+      //   printf("%c", data[i]);
+      // }
       // printf("\n");
+
+      size_t writtenSize = 0;
+      while (writtenSize < readSize) {
+        esp_err_t err =
+            i2s_write(i2s_num, data + writtenSize, readSize - writtenSize,
+                      &writtenSize, pdMS_TO_TICKS(100));
+        // Serial.printf("Wrote %d bytes so far\n", writtenSize);
+        if (err) {
+          Serial.printf("Error while pushing data to DAC: %d\n", err);
+        }
+      }
+
       vRingbufferReturnItem(buffer, (void *)data);
     } else {
       // Serial.println("Failed to read data");
@@ -140,7 +172,7 @@ static void WiFiEvent(WiFiEvent_t event) {
       Serial.print("WiFi connected! IP address: ");
       Serial.println(WiFi.localIP());
 
-      xTaskCreate(tcpServerTask, "Server", 4096, NULL, 1, NULL);
+      xTaskCreate(tcpServerTask, "Server", 8192, NULL, 1, NULL);
       connected = true;
       break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
@@ -171,10 +203,14 @@ void setup() {
     Serial.printf("DAC initialization failed: %d\n", wm_init_result);
   }
 
-  buffer = xRingbufferCreate(1 << 12, RINGBUF_TYPE_BYTEBUF);
+  buffer = xRingbufferCreate(1 << 16, RINGBUF_TYPE_BYTEBUF);
   xRingbufferPrintInfo(buffer);
 
   connectToWiFi(ssid, pwd);
+
+  i2s_driver_install(i2s_num, &i2s_config, 0, NULL);
+  i2s_set_pin(i2s_num, &pin_config);
+  i2s_set_sample_rates(i2s_num, 44100);
 
   xTaskCreate(i2sTask, "I2S", 10000, NULL, 1, NULL);
 }
