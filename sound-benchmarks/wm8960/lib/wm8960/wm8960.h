3c655ce321388d4020ed57c2ba1814c7046d40de
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-09-27 15:35:26 +0200

    Add wm8960 i2s library

---
+++ sound-benchmarks/wm8960/lib/wm8960/wm8960.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SDA_PIN 0
+#define SCL_PIN 0
+
+#define I2C_BUS_NO 0
+
+#define ACK_VAL 0
+#define NACK_VAL 0
+
+#define ACK_CHECK_EN 0
+#define ACK_CHECK_DIS 0
+
+#define CODEC_ADDR 0
---
+++ sound-benchmarks/wm8960/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "wm8960.h"
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
