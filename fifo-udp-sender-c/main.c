c41435ea368ab298eec3817a9f69934279806b53
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-04 17:47:06 +0200

    WIP c bench

---
+++ fifo-udp-sender-c/main.c
@@ -0,0 +1,59 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SERVER "192.168.11.230"
+#define BUFLEN 3200
+#define PORT 8888
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+
+int main(void) {
+    struct sockaddr_in client;
+    int s, slen = sizeof(client);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        die("socket");
+    }
+
+    memset((char *) &client, 0, sizeof(client)); 
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+
+    if (inet_aton(SERVER , &client.sin_addr) == 0) 
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+
+    int fifo = open("/tmp/bassfifo", O_CREAT);
+    if (fifo < 0) {
+        die("fifo");
+    }
+
+    for (int j = 0; j < 441; j++) {
+        ssize_t read_bytes = 0;
+        do {
+            read_bytes += read(fifo, &message[read_bytes], BUFLEN - read_bytes);
+        } while (read_bytes < BUFLEN);
+
+        //printf("Sending\n");
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &client, slen)==-1)
+        {
+            die("sendto()");
+        }
+    }
+}
