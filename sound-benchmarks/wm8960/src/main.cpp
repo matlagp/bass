740a50db720fb5470994460b9221451a5b4f06b1
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-05 22:56:29 +0200

    Add audio sample stretching

--- sound-benchmarks/wm8960/src/main.cpp
+++ sound-benchmarks/wm8960/src/main.cpp
@@ -5,6 +5,7 @@
 #include <WiFiUdp.h>
 #include "wm8960.h"
 #include "sine.h"
+#include "stretch.h"
 
 void connectToWiFi(const char *ssid, const char *pwd);
 void WiFiEvent(WiFiEvent_t event);
@@ -30,7 +31,7 @@ void setup()
 
   init_i2s();
 
-  buffer = xRingbufferCreate(100000, RINGBUF_TYPE_NOSPLIT);
+  buffer = xRingbufferCreate(50000, RINGBUF_TYPE_NOSPLIT);
   if (buffer == NULL)
   {
     Serial.println("Failed to create ring buffer");
@@ -47,7 +48,7 @@ void setup()
   xTaskCreate(
       i2sTask,
       "i2sTask",
-      1024,
+      (1 << 15),
       NULL,
       2,
       NULL);
@@ -107,6 +108,12 @@ void receiveTask(void *pvParameters)
 void i2sTask(void *pvParameters)
 {
   size_t bytes_written;
+  int min_period = 44100 / 333;
+  int max_period = 44100 / 55;
+
+  StretchHandle stretcher = stretch_init(min_period, max_period, 2, 1);
+  uint8_t tx_stretched[(125 * 2 + (44100 / 55) * 3) * 4];
+
   for (;;)
   {
     uint8_t *tx = (uint8_t *)xRingbufferReceive(buffer, &bytes_written, portMAX_DELAY);
@@ -116,11 +123,42 @@ void i2sTask(void *pvParameters)
       continue;
     }
 
-    for (int i = 0; i < 5; i++)
-    {
-      i2s_write(I2S_NUM_0, tx + 100 * i, 100, &bytes_written, portMAX_DELAY);
-    }
+    size_t buffer_size = xRingbufferGetCurFreeSize(buffer);
 
-    vRingbufferReturnItem(buffer, tx);
+    if (buffer_size > 22000)
+    {
+      size_t samples_generated = stretch_samples(stretcher, (short *)tx, 500 / 4, (short *)tx_stretched, 1.5);
+
+      if (samples_generated != 0)
+      {
+        i2s_write(I2S_NUM_0, tx_stretched, samples_generated * 4, &bytes_written, portMAX_DELAY);
+      }
+      else
+      {
+        i2s_write(I2S_NUM_0, tx, 500, &bytes_written, portMAX_DELAY);
+      }
+
+      vRingbufferReturnItem(buffer, tx);
+    }
+    else if (buffer_size < 3000)
+    {
+      size_t samples_generated = stretch_samples(stretcher, (short *)tx, 500 / 4, (short *)tx_stretched, 0.75);
+
+      if (samples_generated != 0)
+      {
+        i2s_write(I2S_NUM_0, tx_stretched, samples_generated * 4, &bytes_written, portMAX_DELAY);
+      }
+      else
+      {
+        i2s_write(I2S_NUM_0, tx, 500, &bytes_written, portMAX_DELAY);
+      }
+
+      vRingbufferReturnItem(buffer, tx);
+    }
+    else
+    {
+      i2s_write(I2S_NUM_0, tx, 500, &bytes_written, portMAX_DELAY);
+      vRingbufferReturnItem(buffer, tx);
+    }
   }
 }
