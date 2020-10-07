94ebc1d98bdf256adf1e2dfb0a8161091f1b20ed
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-07 17:02:34 +0200

    Add mqtt

--- prototype-esp-idf/src/main.c
+++ prototype-esp-idf/src/main.c
@@ -4,9 +4,11 @@
 #include <freertos/event_groups.h>
 #include <freertos/ringbuf.h>
 #include <esp_wifi.h>
+#include <soc/efuse_reg.h>
 #include "nvs_flash.h"
 #include "lwip/sockets.h"
 #include "driver/i2s.h"
+#include "mqtt_client.h"
 
 #include "wm8960.h"
 #include "i2s_setup.h"
@@ -98,6 +100,99 @@ void i2sTask(void *params)
   }
 }
 
+esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+  esp_mqtt_client_handle_t client = event->client;
+  int msg_id;
+
+  switch (event->event_id)
+  {
+  case MQTT_EVENT_CONNECTED:
+  {
+    char *topic_state = malloc(30);
+    char *topic_settings = malloc(35);
+
+    uint64_t chipmacid;
+    ESP_ERROR_CHECK(esp_efuse_mac_get_default((uint8_t *)(&chipmacid)));
+
+    snprintf(topic_state, 30, "/nodes/%08X/state", (uint32_t)(chipmacid >> 16));
+    snprintf(topic_settings, 35, "/nodes/%08X/settings/#", (uint32_t)(chipmacid >> 16));
+
+    ESP_LOGI("mqtt", "MQTT_EVENT_CONNECTED");
+    msg_id = esp_mqtt_client_publish(client, topic_state, "192.168.11.117", 0, 1, 1);
+    ESP_LOGI("mqtt", "sent publish successful, msg_id=%d", msg_id);
+
+    msg_id = esp_mqtt_client_subscribe(client, topic_settings, 1);
+    ESP_LOGI("mqtt", "sent subscribe successful, msg_id=%d", msg_id);
+    break;
+  }
+  case MQTT_EVENT_DISCONNECTED:
+    ESP_LOGI("mqtt", "MQTT_EVENT_DISCONNECTED");
+    break;
+
+  case MQTT_EVENT_SUBSCRIBED:
+    ESP_LOGI("mqtt", "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+    msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+    ESP_LOGI("mqtt", "sent publish successful, msg_id=%d", msg_id);
+    break;
+  case MQTT_EVENT_UNSUBSCRIBED:
+    ESP_LOGI("mqtt", "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+    break;
+  case MQTT_EVENT_PUBLISHED:
+    ESP_LOGI("mqtt", "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+    break;
+  case MQTT_EVENT_DATA:
+    ESP_LOGI("mqtt", "MQTT_EVENT_DATA");
+    printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+    printf("DATA=%.*s\r\n", event->data_len, event->data);
+    break;
+  case MQTT_EVENT_ERROR:
+    ESP_LOGI("mqtt", "MQTT_EVENT_ERROR");
+    break;
+  default:
+    ESP_LOGI("mqtt", "Other event id:%d", event->event_id);
+    break;
+  }
+  return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+  ESP_LOGD("mqtt", "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+  mqtt_event_handler_cb(event_data);
+}
+
+void mqttTask(void *params)
+{
+  char *name = malloc(14);
+  char *topic_state = malloc(30);
+  char *topic_settings = malloc(35);
+
+  uint64_t chipmacid;
+  ESP_ERROR_CHECK(esp_efuse_mac_get_default((uint8_t *)(&chipmacid)));
+
+  snprintf(name, 14, "node-%08X", (uint32_t)(chipmacid >> 16));
+  snprintf(topic_state, 30, "/nodes/%08X/state", (uint32_t)(chipmacid >> 16));
+  snprintf(topic_settings, 35, "/nodes/%08X/settings/#", (uint32_t)(chipmacid >> 16));
+
+  ESP_LOGE("mqtt", "Registering as %s", name);
+
+  const esp_mqtt_client_config_t mqtt_cfg = {
+      .uri = "mqtt://192.168.11.113",
+      .port = 1883,
+      .lwt_topic = topic_state,
+      .lwt_qos = 0,
+      .lwt_msg = "0",
+      .lwt_retain = true};
+  esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+  ESP_ERROR_CHECK(esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client));
+
+  ESP_ERROR_CHECK(esp_mqtt_client_start(client));
+
+  ESP_LOGI("mqtt", "Mqtt connection started");
+  vTaskDelete(NULL);
+}
+
 void waitingTask(void *params)
 {
   EventBits_t uxBits;
@@ -111,6 +206,7 @@ void waitingTask(void *params)
       ESP_LOGI("wait", "WiFi Connected to ap");
       xTaskCreate(udpTask, "udp_server", 4096, NULL, 5, NULL);
       xTaskCreate(i2sTask, "i2s_task", 4096, NULL, 5, NULL);
+      xTaskCreate(mqttTask, "mqtt_task", 4096, NULL, 5, NULL);
       vTaskDelete(NULL);
     }
   }
