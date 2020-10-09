b29caf1f6b1d1914542128cd8faaedf08cc4d892
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-09 19:31:34 +0200

    Start refactoring wifi task

---
+++ prototype-esp-idf/src/wifi_task.h
@@ -0,0 +1,15 @@
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+#include <freertos/task.h>
+#include <nvs_flash.h>
+
+#ifndef WIFI_TASK_H
+#define WIFI_TASK_H
+
+#define WIFI_TASK_TAG "wifiTask"
+
+void createWifiTask();
+
+#endif
