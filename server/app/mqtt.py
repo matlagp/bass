166cba0575ab3c2ddc45482a6481f27258c91094
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-18 20:54:04 +0200

    Refactor server mqtt validation

--- server/app/mqtt.py
+++ server/app/mqtt.py
@@ -24,44 +24,43 @@ class MQTTClient(threading.Thread):
 
     def _on_message(client, userdata, message):
         try:
-            print(f"MESSAGE({message.topic}): {message.payload}")
+            print(f"MQTT message: ({message.topic}): {message.payload}")
             topic = message.topic.split('/')[1:]
             node_id = int(topic[1], 16)
+            msg = message.payload.decode('ascii')
+
             if topic[2] == 'state':
                 node = Node(node_id, message.payload.decode('ascii'))
                 __class__.node_repository.create(node)
 
-            if topic[2] == 'settings' and topic[3] == 'volume':
-                volume = int(message.payload.decode('ascii'))
+            elif topic[2] == 'settings':
                 node = __class__.node_repository.find(node_id)
-                if volume < 0 or volume > 100:
-                    raise ValueError("Volume not between 0 and 100")
-                node.volume = volume
-                __class__.node_repository.update(node)
 
-            if topic[2] == 'settings' and topic[3] == 'bass':
-                bass = float(message.payload.decode('ascii'))
-                node = __class__.node_repository.find(node_id)
-                if bass < -24 or bass > 12:
-                    raise ValueError("Bass not between -24 and 12")
-                node.bass = bass
-                __class__.node_repository.update(node)
+                if topic[3] == 'volume':
+                    volume = int(msg)
+                    __class__._check_range(volume, 0, 100, "Volume")
+                    node.volume = volume
 
-            if topic[2] == 'settings' and topic[3] == 'mid':
-                mid = float(message.payload.decode('ascii'))
-                node = __class__.node_repository.find(node_id)
-                if mid < -24 or mid > 12:
-                    raise ValueError("Mid not between -24 and 12")
-                node.mid = mid
-                __class__.node_repository.update(node)
+                elif topic[3] == 'bass':
+                    bass = float(msg)
+                    __class__._check_range(bass, -24, 12, "Bass")
+                    node.bass = bass
 
-            if topic[2] == 'settings' and topic[3] == 'trebble':
-                trebble = float(message.payload.decode('ascii'))
-                node = __class__.node_repository.find(node_id)
-                if trebble < -24 or trebble > 12:
-                    raise ValueError("Trebble not between -24 and 12")
-                node.trebble = trebble
+                elif topic[3] == 'mid':
+                    mid = float(msg)
+                    __class__._check_range(mid, -24, 12, "Mid")
+                    node.mid = mid
+
+                elif topic[3] == 'trebble':
+                    trebble = float(msg)
+                    __class__._check_range(trebble, -24, 12, "Trebble")
+                    node.trebble = trebble
+
                 __class__.node_repository.update(node)
 
         except Exception as e:
-            print(f"MQTT MSG ERROR: {e}")
+            print(f"MQTT message handling error: {e}")
+
+    def _check_range(value, min_value, max_value, comment):
+        if value < min_value or value > max_value:
+            raise ValueError(f"{comment}({value}) not between {min_value} and {max_value}")
