085e5efb70825cf2ef7cfdb9438a7ba9399b5d02
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-18 19:32:24 +0200

    Refactor gstreamer pipeline

--- streamer/mqtt.py
+++ streamer/mqtt.py
@@ -20,50 +20,52 @@ class MQTTClient(Thread):
 
     def _on_message(self, client, userdata, message):
         try:
-            print(f"MESSAGE({message.topic}): {message.payload}")
+            print(f"MQTT message: ({message.topic}): {message.payload}")
             topic = message.topic.split('/')[1:]
             node_id = int(topic[1], 16)
+            msg = message.payload.decode('ascii')
 
             if topic[2] == 'state':
-               self._on_state(node_id, message.payload.decode('ascii'))
+               self._on_state(node_id, msg)
 
-            if topic[2] == 'settings' and topic[3] == 'volume':
-                self._on_volume(node_id, int(message.payload.decode('ascii')))
+            elif topic[2] == 'settings':
+                if topic[3] == 'volume':
+                    self._on_volume(node_id, int(msg))
 
-            if topic[2] == 'settings' and topic[3] == 'bass':
-                self._on_bass(node_id, float(message.payload.decode('ascii')))
+                elif topic[3] == 'bass':
+                    self._on_bass(node_id, float(msg))
 
-            if topic[2] == 'settings' and topic[3] == 'mid':
-                self._on_mid(node_id, float(message.payload.decode('ascii')))
+                elif topic[3] == 'mid':
+                    self._on_mid(node_id, float(msg))
 
-            if topic[2] == 'settings' and topic[3] == 'trebble':
-                self._on_trebble(node_id, float(message.payload.decode('ascii')))
+                elif topic[3] == 'trebble':
+                    self._on_trebble(node_id, float(msg))
 
         except Exception as e:
             print(f"MQTT message handling error: {e}")
 
-    def _on_state(self, node_id, message):
-        if message == '0':
+    def _on_state(self, node_id, ip):
+        if len(ip) < 2: # '0' or '' or something else that's invalid
             self.pipeline.remove_node(node_id)
         else:
-            self.pipeline.add_node(node_id, message)
+            self.pipeline.add_node(node_id, ip)
 
     def _on_volume(self, node_id, volume):
-        if volume < 0 and volume > 100:
-            raise ValueError("Volume not between 0 and 100")
+        __class__._check_range(volume, 0, 100, "Volume")
         self.pipeline.set_volume(node_id, volume)
 
     def _on_bass(self, node_id, bass):
-        if bass < -24 and bass > 12:
-            raise ValueError("Bass not between -24 and 12")
+        __class__._check_range(bass, -24, 12, "Bass")
         self.pipeline.set_bass(node_id, bass)
 
     def _on_mid(self, node_id, mid):
-        if mid < -24 and mid > 12:
-            raise ValueError("Mid not between -24 and 12")
+        __class__._check_range(mid, -24, 12, "Mid")
         self.pipeline.set_mid(node_id, mid)
 
     def _on_trebble(self, node_id, trebble):
-        if trebble < -24 and trebble > 12:
-            raise ValueError("Trebble not between -24 and 12")
+        __class__._check_range(trebble, -24, 12, "Trebble")
         self.pipeline.set_trebble(node_id, trebble)
+
+    def _check_range(value, min_value, max_value, comment):
+        if value < min_value or value > max_value:
+            raise ValueError(f"{comment}({value}) not between {min_value} and {max_value}")
--- streamer/pipeline.py
+++ streamer/pipeline.py
@@ -1,78 +1,73 @@
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
+from utils import *
 
 BUFFER_SIZE = 512
+
 
 class Pipeline(object):
     def __init__(self):
         self.nodes = {}
         self.pipeline = Gst.Pipeline()
 
-        self.src = Gst.ElementFactory.make('alsasrc')
-        self.tee = Gst.ElementFactory.make('tee')
-        self.fakequeue = Gst.ElementFactory.make('queue')
-        self.fakesink = Gst.ElementFactory.make('fakesink')
+        self.src = make_element('alsasrc')
+        self.tee = make_element('tee')
+        self.fakequeue = make_element('queue')
+        self.fakesink = make_element('fakesink')
 
-        self.pipeline.add(self.src)
-        self.pipeline.add(self.tee)
-        self.pipeline.add(self.fakequeue)
-        self.pipeline.add(self.fakesink)
+        add_to_pipeline(
+            self.pipeline,
+            [self.src, self.tee, self.fakequeue, self.fakesink]
+        )
 
-        self.src.link(self.tee)
-        self.tee.get_request_pad('src_%u').link(self.fakequeue.get_static_pad('sink'))
-        self.fakequeue.link(self.fakesink)
+        link([self.src, self.tee])
+        link_tee(self.tee, self.fakequeue)
+        link([self.fakequeue, self.fakesink])
 
         self.fakesink.set_property('sync', False)
+
+    def get_node(self, node_id):
+        if node_id not in self.nodes:
+            self.nodes[node_id] = Node(self, node_id)
+        return self.nodes[node_id]
+
+    def add_node(self, node_id, host):
+        node = self.get_node(node_id)
+        node.set_host(host)
+        node.attach()
+        print(self.nodes)
+
+    def remove_node(self, node_id):
+        node = self.get_node(node_id)
+        node.detach()
+        print(self.nodes)
+
+    def set_volume(self, node_id, volume):
+        node = self.get_node(node_id)
+        node.set_volume(volume)
+        print(self.nodes)
+
+    def set_bass(self, node_id, bass):
+        node = self.get_node(node_id)
+        node.set_bass(bass)
+        print(self.nodes)
+
+    def set_mid(self, node_id, mid):
+        node = self.get_node(node_id)
+        node.set_mid(mid)
+        print(self.nodes)
+
+    def set_trebble(self, node_id, trebble):
+        node = self.get_node(node_id)
+        node.set_trebble(trebble)
+        print(self.nodes)
 
     def pause(self):
         return self.pipeline.set_state(Gst.State.PAUSED)
 
     def play(self):
         return self.pipeline.set_state(Gst.State.PLAYING)
-
-    def add_node(self, node_id, host):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.set_host(host)
-        node.attach()
-        print(self.nodes)
-
-    def remove_node(self, node_id):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.detach()
-        print(self.nodes)
-
-    def set_volume(self, node_id, volume):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.set_volume(volume)
-        print(self.nodes)
-
-    def set_bass(self, node_id, bass):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.set_bass(bass)
-        print(self.nodes)
-
-    def set_mid(self, node_id, mid):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.set_mid(mid)
-        print(self.nodes)
-
-    def set_trebble(self, node_id, trebble):
-        if node_id not in self.nodes:
-            self.nodes[node_id] = Node(self, node_id)
-        node = self.nodes[node_id]
-        node.set_trebble(trebble)
-        print(self.nodes)
 
 
 class Node(object):
@@ -83,9 +78,18 @@ class Node(object):
         self.attached = False
 
         self.vol = 100
-        self.bass = 0
-        self.mid = 0
-        self.trebble = 0
+        self.bass = 0.0
+        self.mid = 0.0
+        self.trebble = 0.0
+
+    def __repr__(self):
+        return f"Node#{self.node_id}"\
+            f"(attached: {self.attached}, "\
+            f"host: {self.host}, "\
+            f"vol: {self.vol}, "\
+            f"bass: {self.bass}, "\
+            f"mid: {self.mid}, "\
+            f"trebble: {self.trebble})"
 
     def set_volume(self, volume):
         self.vol = volume
@@ -115,11 +119,11 @@ class Node(object):
     def attach(self):
         if self.attached: return
 
-        self.latency = Gst.ElementFactory.make('queue')
-        self.equalizer = Gst.ElementFactory.make('equalizer-3bands')
-        self.volume = Gst.ElementFactory.make('volume')
-        self.buffersize = Gst.ElementFactory.make('rndbuffersize')
-        self.sink = Gst.ElementFactory.make('udpsink')
+        self.latency = make_element('queue')
+        self.equalizer = make_element('equalizer-3bands')
+        self.volume = make_element('volume')
+        self.buffersize = make_element('rndbuffersize')
+        self.sink = make_element('udpsink')
 
         self.buffersize.set_property('min', BUFFER_SIZE)
         self.buffersize.set_property('max', BUFFER_SIZE)
@@ -127,23 +131,14 @@ class Node(object):
         self.sink.set_property('host', self.host)
         self.sink.set_property('port', self.port)
 
-        self.pipeline.pipeline.add(self.latency)
-        self.pipeline.pipeline.add(self.equalizer)
-        self.pipeline.pipeline.add(self.volume)
-        self.pipeline.pipeline.add(self.buffersize)
-        self.pipeline.pipeline.add(self.sink)
+        elements = [self.latency, self.equalizer, self.volume, self.buffersize, self.sink]
 
-        self.latency.link(self.equalizer)
-        self.equalizer.link(self.volume)
-        self.volume.link(self.buffersize)
-        self.buffersize.link(self.sink)
+        add_to_pipeline(self.pipeline.pipeline, elements)
+        link(elements)
 
         self.pipeline.pause()
 
-        self.tee = self.pipeline.tee.get_request_pad('src_%u')
-        self.tee.link(self.latency.get_static_pad('sink'))
-
-        self.pipeline.play()
+        self.tee = link_tee(self.pipeline.tee, self.latency)
 
         self.attached = True
 
@@ -152,43 +147,21 @@ class Node(object):
         self.set_mid(self.mid)
         self.set_trebble(self.trebble)
 
-    def detach(self):
-        if not self.attached: return
-
-        self.pipeline.pause()
-
-        self.tee.unlink(self.latency.get_static_pad('sink'))
-
-        self.pipeline.tee.release_request_pad(self.tee)
-
         self.pipeline.play()
 
-        self.latency.unlink(self.equalizer)
-        self.equalizer.unlink(self.volume)
-        self.volume.unlink(self.buffersize)
-        self.buffersize.unlink(self.sink)
+    def detach(self):
+        if not self.attached:
+            return
 
-        self.pipeline.pipeline.remove(self.latency)
-        self.pipeline.pipeline.remove(self.equalizer)
-        self.pipeline.pipeline.remove(self.volume)
-        self.pipeline.pipeline.remove(self.buffersize)
-        self.pipeline.pipeline.remove(self.sink)
+        self.pipeline.pause()
+        unlink_tee(self.pipeline.tee, self.tee, self.latency)
+        self.pipeline.play()
 
-        self.latency.set_state(Gst.State.NULL)
-        self.equalizer.set_state(Gst.State.NULL)
-        self.volume.set_state(Gst.State.NULL)
-        self.buffersize.set_state(Gst.State.NULL)
-        self.sink.set_state(Gst.State.NULL)
-
-        self.latency.unref()
-        self.equalizer.unref()
-        self.volume.unref()
-        self.buffersize.unref()
-        self.sink.unref()
-
-        self.latency = None
-        self.equalizer = None
-        self.sink = None
-        self.tee = None
+        elements = [self.latency, self.equalizer, self.volume, self.buffersize, self.sink]
+        unlink(elements)
+        remove_from_pipeline(self.pipeline.pipeline, elements)
+        set_null_state(elements)
+        unref(elements)
+        self.latency, self.equalizer, self.volume, self.sink, self.tee = [ None for _ in elements ]
 
         self.attached = False
