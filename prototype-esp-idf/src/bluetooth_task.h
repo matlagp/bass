825f0d57d5d5702edbd89ebc3860f93db3f3c072
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-10-19 16:23:05 +0200

    Add some bluetooth docs

--- prototype-esp-idf/src/bluetooth_task.h
+++ prototype-esp-idf/src/bluetooth_task.h
@@ -7,16 +7,30 @@
 #include <esp_spp_api.h>
 #include "lwip/sockets.h"
 
-#include "time.h"
-#include "sys/time.h"
-
 #ifndef BLUETOOTH_TASK_H
 #define BLUETOOTH_TASK_H
 
 #define BLUETOOTH_TASK_TAG "btTask"
 
+/*
+Initializes the bluetooth SPP server that receives WiFi credentials.
+
+@param on_credentials_received A function called after successfully receiving WiFi credentials
+*/
 void createBluetoothTask(void (*on_credentials_received)(char *ssid, char *password, char *ip));
+
+/*
+Restarts the SPP server to retrieve WiFi credentials again.
+Call after checking that previous credentials are invalid.
+*/
 void retryBluetooth(void);
+
+/*
+Cleans up the whole bluetooth stack.
+Call after checking that the retrieved credentials are valid.
+
+The bluetooth SPP server cannot be started again after calling this.
+*/
 void cleanupBluetooth(void);
 
 #endif
